e022b62eb181015f6425d43e1579e5ad965cb328
Alejandro <smarketing.cuz@gmail.com>
2024-07-12 16:19:46 +0200

    project update

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="relative flex flex-col justify-between items-center gap-20 min-h-screen mx-auto md:p-24">
+    <main className="relative flex flex-col items-center gap-20 min-h-screen mx-auto md:p-24">
       <Suspense fallback={<div>Loading...</div>}>
         <div className=" flex justify-center pt-10 md:pt-0 z-10 max-w-5xl w-full lg:items-center lg:justify-between font-mono text-sm lg:flex">
           <div className="absolute bottom-0 left-0 flex w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -32,7 +32,7 @@ export default function Home() {
           <ConnectWalletButton />
         </div>
 
-        <div className=" w-fit flex flex-col gap-3 place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[200px] md:after:w-[350px] after:translate-x-1/3 after:bg-gradient-conic after:from-green-200 after:via-green-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-green-700 before:dark:opacity-10 after:dark:from-green-900 after:dark:via-[#09ff01] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <div className="flex flex-col mt-52 items-center">
           <span className="text-3xl font-bold">Web3 Starter template</span>
           {isConnected && (
             <span className="text-sm font-mono font-medium max-w-md text-center text-gray-500">
@@ -47,25 +47,6 @@ export default function Home() {
               <div>Linea block number: {Number(blockNumber)}</div>
             )}
           </div>
-        </div>
-
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <a
-            href="#"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Templates{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Explore starter templates for Web3 using Consensys products.
-            </p>
-          </a>
         </div>
       </Suspense>
     </main>
--- providers/WagmiProvider.tsx
+++ providers/WagmiProvider.tsx
@@ -1,10 +1,13 @@
-import { WagmiProvider } from "wagmi";
+"use client";
+
+import { WagmiProvider, deserialize, serialize } from "wagmi";
 import { config } from "@/wagmi.config";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MetaMaskProvider } from "@metamask/sdk-react";
+import { QueryClient } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 
 import { createPublicClient, http } from "viem";
 import { linea } from "viem/chains";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 export const client = createPublicClient({
   chain: linea,
@@ -15,29 +18,34 @@ interface WagmiProviderProps {
   children: React.ReactNode;
 }
 
-const queryClient = new QueryClient();
-
 const Provider: React.FC<WagmiProviderProps> = ({ children }) => {
-  const host =
-    typeof window !== "undefined" ? window.location.host : "defaultHost";
-
-  const sdkOptions = {
-    logging: { developerMode: false },
-    checkInstallationImmediately: false,
-    dappMetadata: {
-      name: "RAD Starter",
-      url: host,
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        gcTime: 1_000 * 60 * 60 * 24, // 24 hours
+        networkMode: "offlineFirst",
+        refetchOnWindowFocus: false,
+        retry: 0,
+      },
+      mutations: { networkMode: "offlineFirst" },
     },
-  };
+  });
+
+  const persister = createSyncStoragePersister({
+    serialize,
+    storage: window.localStorage,
+    deserialize,
+  });
 
   return (
-    <MetaMaskProvider debug={false} sdkOptions={sdkOptions}>
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </WagmiProvider>
-    </MetaMaskProvider>
+    <WagmiProvider config={config}>
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ persister }}
+      >
+        {children}
+      </PersistQueryClientProvider>
+    </WagmiProvider>
   );
 };
 
